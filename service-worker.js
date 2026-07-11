600bff22013609d13237069beb46e04b0abc4e72
thkim3589-web <thkim3589@gmail.com>
2026-07-12 02:55:07 +0900

    Refactor service worker to simplify install and fetch
    Removed caching logic and added skipWaiting in install event.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,5 @@
-const CACHE_NAME='photo-manager-v1';
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+
+self.addEventListener('fetch', e => {});
